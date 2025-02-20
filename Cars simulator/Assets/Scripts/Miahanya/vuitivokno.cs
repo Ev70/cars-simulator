0f64f7693c62445aabe54d9ffb84e47631c1913c
Mishanya <mishanya2009.31@gmail.com>
2025-02-20 14:05:47 +1000

    added quit button in start scene

---
+++ Cars simulator/Assets/Scripts/Miahanya/vuitivokno.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vuitivokno : MonoBehaviour
+{
+    public void goOut()
+    {
+        Application.Quit();
+    }
+}
